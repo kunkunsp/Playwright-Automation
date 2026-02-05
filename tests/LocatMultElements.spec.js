2c681d2fa6375efb9383c3ce1b1d59de35c079bd
Silvana <silvanaotiniano@hotmail.com>
2026-02-05 16:40:49 +0100

    mejora de localizadores

--- tests/LocatMultElements.spec.js
+++ tests/LocatMultElements.spec.js
@@ -4,13 +4,23 @@ test ('LocatMultElements', async ({page})=>{
 
     await page.goto('https://parabank.parasoft.com/parabank/index.htm;jsessionid=ABDD89053867AF9353509CD21D1FE2B5')
 
-    const links = await page.$$('a');
+    /*const links = await page.$$('a');
     
     for(const link of links)
     {
         const linktest= await link.textContent();
         console.log(linktest);
 
-    }
+    }*/
+//Locate all the products display on home page
 
+page.waitForSelector("//div[@id='rightPanel']//ul//li/a"); //another method call
+
+const products = await page.$$("//div[@id='rightPanel']//ul//li/a")
+
+    for (const product of products) {
+        const prodName = await product.textContent();
+        console.log(prodName);
+    }
+    
 })
